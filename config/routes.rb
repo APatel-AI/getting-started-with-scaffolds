0f5d7048de7c2fccf6fca5f4556b837872d7517a
Arpan Patel <arpan.p.se@gmail.com>
2024-06-26 16:29:51 +0000

    updated movies controller and the naming convention for the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Routes for the Movie resource:
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  
+  get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  
+  patch("/movies/:path_id", { :controller => "movies", :action => "update" })
+  
+  # DELETE
+  delete("/movies/:path_id", { :controller => "movies", :action => "destroy" })
+
+  #------------------------------
+
 end
